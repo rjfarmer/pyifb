ff8bec491d95c93b15a7a0608627a919c7538aaa
Robert Farmer <robert.j.farmer37@gmail.com>
2024-02-11 20:56:49 +0000

    Search for iso_fortran_binding.h

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup, Extension
+import pathlib
+import functools
 
+@functools.cache
+def get_include():
+    p = list(pathlib.Path('/usr').glob('**/ISO_Fortran_binding.h'))[0]
+    return p.parent
 
 
 setup(
     ext_modules=[
         Extension(
-            name="pyifb.ifb",
-            sources=["pyifb/ifb.c",],
+            name = "pyifb.ifb",
+            sources = ["pyifb/ifb.c",],
+            include_dirs = [get_include()]
         )
     ],
 )
