dc90b5ca46c4c85b61f13e99a4fef65d3b82e52a
Robert Farmer <robert.j.farmer37@gmail.com>
2025-07-02 21:55:34 +0100

    Remove manual search

--- setup.py
+++ setup.py
@@ -2,18 +2,13 @@ from setuptools import setup, Extension
 import pathlib
 import functools
 
-@functools.lru_cache(None)
-def get_include():
-    p = list(pathlib.Path('/usr').glob('**/ISO_Fortran_binding.h'))[0]
-    return p.parent
-
-
 setup(
     ext_modules=[
         Extension(
-            name = "pyifb.ifb",
-            sources = ["pyifb/ifb.c",],
-            include_dirs = [get_include()]
+            name="pyifb.ifb",
+            sources=[
+                "pyifb/ifb.c",
+            ],
         )
     ],
 )
